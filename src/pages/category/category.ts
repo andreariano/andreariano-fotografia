976b937ec21f56c10acea71d989fded921dad422
andreariano <andreariano@yahoo.com.br>
2017-06-08 02:18:08 -0300

    WIP: added category page

---
+++ src/pages/category/category.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-category',
+  templateUrl: 'category.html'
+})
+export class Category {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
